fb68bd5296dfb708d467730c9c500390ab36e254
Shumyla <shumyla.fahd@hotmail.com>
2020-09-07 15:58:04 +0100

    added express function

---
+++ calculator.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hello World")
+}
+);
+
+app.listen(3000, function() {
+    console.log("Server is listening on port 3000")
+});
+
+
